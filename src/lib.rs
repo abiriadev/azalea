166830ec918d8a256cf67937c0f889dd80686c4b
Abiria <abiria.dev@gmail.com>
2023-09-08 11:43:13 +0900

    feat: add passing implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 use std::collections::HashMap;
 
+use regex::Regex;
+use tl::{parse, Node, ParserOptions};
+
 pub struct Template {
 	src: String,
 }
@@ -8,6 +11,27 @@ impl Template {
 	pub fn new(src: String) -> Self { Self { src } }
 
 	pub fn render(&self, props: &HashMap<&'static str, String>) -> String {
-		todo!()
+		// TODO: return Err if parsing fail
+		let mut vdom = parse(&self.src, ParserOptions::default()).unwrap();
+
+		for node in vdom.nodes_mut() {
+			let bytes = match node {
+				Node::Raw(r) => r,
+				_ => continue,
+			};
+
+			let text = bytes.try_as_utf8_str().unwrap();
+
+			let re = Regex::new(r"\{\{\s*(\w+)\s*\}\}").unwrap();
+			let caps = re.captures(text).unwrap();
+
+			let Some(prop) = caps.get(1) else { continue; };
+
+			let Some(value) = props.get(prop.as_str()) else { continue; };
+
+			bytes.set(value.as_str()).unwrap();
+		}
+
+		vdom.outer_html()
 	}
 }
