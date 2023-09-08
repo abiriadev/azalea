c1cef67de54c5bd1179e0c23c1acf3703f57d52e
Abiria <abiria.dev@gmail.com>
2023-09-08 12:07:17 +0900

    feat: add `Error` enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 use std::{collections::HashMap, sync::OnceLock};
 
 use regex::Regex;
-use tl::{parse, Node, ParserOptions};
+use thiserror::Error;
+use tl::{errors::SetBytesError, parse, Node, ParseError, ParserOptions};
 
 static PROP_REGEX: OnceLock<Regex> = OnceLock::new();
 
@@ -9,33 +10,42 @@ pub struct Template {
 	src: String,
 }
 
+#[derive(Debug, Error)]
+pub enum Error {
+	#[error("{0}")]
+	ParseError(#[from] ParseError),
+
+	#[error("{0}")]
+	SetBytesError(#[from] SetBytesError),
+}
+
 impl Template {
 	pub fn new(src: String) -> Self { Self { src } }
 
-	pub fn render(&self, props: &HashMap<&'static str, String>) -> String {
+	pub fn render(
+		&self,
+		props: &HashMap<&'static str, String>,
+	) -> Result<String, Error> {
 		// TODO: return Err if parsing fail
-		let mut vdom = parse(&self.src, ParserOptions::default()).unwrap();
+		let mut vdom = parse(&self.src, ParserOptions::default())?;
 
 		let prop_regex = PROP_REGEX
 			.get_or_init(|| Regex::new(r"\{\{\s*(\w+)\s*\}\}").unwrap());
 
 		for node in vdom.nodes_mut() {
-			let bytes = match node {
-				Node::Raw(r) => r,
-				_ => continue,
-			};
+			let Node::Raw(bytes) = node else { continue; };
 
-			let text = bytes.try_as_utf8_str().unwrap();
+			let Some(text) = bytes.try_as_utf8_str() else { continue; };
 
-			let caps = prop_regex.captures(text).unwrap();
+			let Some(caps) = prop_regex.captures(text) else { continue; };
 
 			let Some(prop) = caps.get(1) else { continue; };
 
 			let Some(value) = props.get(prop.as_str()) else { continue; };
 
-			bytes.set(value.as_str()).unwrap();
+			bytes.set(value.as_str())?;
 		}
 
-		vdom.outer_html()
+		Ok(vdom.outer_html())
 	}
 }
